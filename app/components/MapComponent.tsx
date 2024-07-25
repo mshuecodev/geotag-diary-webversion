b16c0fc793d08fea50dc2b7dcaac1f9d49109f9c
misna04 <misnasyari@gmail.com>
2024-07-25 19:53:59 +0800

    map preview  fixed

--- app/components/MapComponent.tsx
+++ app/components/MapComponent.tsx
@@ -1,10 +1,36 @@
+"use client"
 import React from "react"
-import { MapContainer, TileLayer, useMap } from "react-leaflet"
+import "leaflet/dist/leaflet.css"
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css"
+import "leaflet-defaulticon-compatibility"
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
+
+import { useWindowDimensions } from "@/app/hooks/useWindowDimensions"
 
 const MapComponent: React.FC = () => {
-	const position = [51.505, -0.09]
+	const { width, height } = useWindowDimensions()
 
-	return <div></div>
+	const position: [number, number] = [51.505, -0.09]
+
+	return (
+		<MapContainer
+			center={position}
+			zoom={11}
+			scrollWheelZoom={true}
+			// style={{ height: "400px", width: "600px" }}
+			style={{ height: "100vh", width: width }}
+		>
+			<TileLayer
+				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+			/>
+			<Marker position={position}>
+				<Popup>
+					This Marker icon is displayed correctly with <i>leaflet-defaulticon-compatibility</i>.
+				</Popup>
+			</Marker>
+		</MapContainer>
+	)
 }
 
 export default MapComponent
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-"use client"
-
 import MapComponent from "@/app/components/MapComponent"
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+		<main className="flex">
 			<div className="map">
 				<MapComponent />
 			</div>
