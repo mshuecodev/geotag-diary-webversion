3ae90b0307aa177c98c2e5a4823c54c6c3e4aabb
misna04 <misnasyari@gmail.com>
2024-07-25 19:41:10 +0800

    check

---
+++ app/components/MapComponent.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { MapContainer, TileLayer, useMap } from "react-leaflet"
+
+const MapComponent: React.FC = () => {
+	const position = [51.505, -0.09]
+
+	return <div></div>
+}
+
+export default MapComponent
